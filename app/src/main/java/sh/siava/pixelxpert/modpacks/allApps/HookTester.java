7ae94ae861d44146458adafa53a6c07244b636bc
Siavash <siavash@gmail.com>
2024-10-23 08:42:58 +0330

    code cleanup

--- app/src/main/java/sh/siava/pixelxpert/modpacks/allApps/HookTester.java
+++ app/src/main/java/sh/siava/pixelxpert/modpacks/allApps/HookTester.java
@@ -1,5 +1,7 @@
 package sh.siava.pixelxpert.modpacks.allApps;
 
+import static sh.siava.pixelxpert.modpacks.Constants.*;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +9,6 @@ import android.content.IntentFilter;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.pixelxpert.BuildConfig;
-import sh.siava.pixelxpert.modpacks.Constants;
 import sh.siava.pixelxpert.modpacks.XposedModPack;
 
 public class HookTester extends XposedModPack {
@@ -24,7 +25,7 @@ public class HookTester extends XposedModPack {
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				new Thread(() -> {
-					Intent broadcast = new Intent(Constants.ACTION_XPOSED_CONFIRMED);
+					Intent broadcast = new Intent(ACTION_XPOSED_CONFIRMED);
 
 					broadcast.putExtra("packageName", lpParam.packageName);
 
@@ -35,10 +36,9 @@ public class HookTester extends XposedModPack {
 			}
 		};
 
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(Constants.ACTION_CHECK_XPOSED_ENABLED);
-
-		mContext.registerReceiver(broadcastReceiver, filter, Context.RECEIVER_EXPORTED);
+		mContext.registerReceiver(broadcastReceiver,
+				new IntentFilter(ACTION_CHECK_XPOSED_ENABLED),
+				Context.RECEIVER_EXPORTED);
 	}
 
 	@Override
